3c6e2356d6f838788345ef1a1b423069bcd9b506
Harold <haroldcj95@gmail.com>
2018-11-07 01:26:09 -0400

    añadir funciones de comprobacion de cajas para tablero ruby

---
+++ lib/tablero.rb
@@ -0,0 +1,84 @@
+class Tablero
+    def initialize()
+        @arregloFilas=true
+        @arregloColumnas=true
+        @columnas= Array.new(4) { Array.new(4, 0) }
+        @filas= Array.new(4) { Array.new(4, 0) }
+    end
+
+    def arregloFilasEstaVacio()
+        return @arregloFilas
+    end
+
+    def arregloColumnasEstaVacio()
+        return @arregloColumnas
+    end
+
+    def insertarFila(posX, posY)
+        @filas[posX][posY]=true
+        @arregloFilas=false
+    end
+
+    def insertarColumna(posX, posY)
+        @columnas[posX][posY]=true
+        @arregloColumnas=false
+    end
+
+    def comprobarSiExisteColumnaDibujadaEnPos(posX,posY)
+        return @columnas[posX][posY]
+    end
+
+    def verificarSiSeFormaUnaCajaAbajoConFila(posX,posY)
+        puntaje=0
+        if(posX<3)
+            if(@filas[posX][posY+1]==true)
+                if(@columnas[posX][posY]==true)
+                    if(@columnas[posX+1][posY+1]==true)
+                        puntaje+=1
+                    end
+                end
+            end
+        end
+        return puntaje
+    end
+
+    def verificarSiSeFormaUnaCajaArribaConFila(posX,posY)
+        puntaje=0
+        if(posX>0)
+            if(@filas[posX][posY-1]==true)
+                if(@columnas[posX][posY-1]==true)
+                    if(@columnas[posX+1][posY-1]==true)
+                        puntaje+=1
+                    end
+                end
+            end
+        end
+        return puntaje
+    end
+    def verificarSiSeFormaUnaCajaIzquierdaConColumna(posX,posY)
+        puntaje=0
+        if(posY>0)
+            if(@columnas[posX-1][posY]==true)
+                if(@filas[posX-1][posY]==true)
+                    if(@filas[posX-1][posY+1]==true)
+                        puntaje+=1
+                    end
+                end
+            end
+        end
+        return puntaje
+    end
+    def verificarSiSeFormaUnaCajaDerechaConColumna(posX,posY)
+        puntaje=0
+        if(posY<3)
+            if(@columnas[posX+1][posY]==true)
+                if(@filas[posX][posY]==true)
+                    if(@filas[posX][posY+1]==true)
+                        puntaje+=1
+                    end
+                end
+            end
+        end
+        return puntaje
+    end
+end
--- spec/tablero_spec.rb
+++ spec/tablero_spec.rb
@@ -3,11 +3,29 @@ describe Tablero do
     before(:each) do
         @tb=Tablero.new()
     end
-    it "deberia estar vacia al momento de crearla" do
-      expect(@tb.estaVacia()).to eq true
+    it "filas y columnas deberian estar vacias al crearlas" do
+      expect(@tb.arregloFilasEstaVacio()).to eq true
+      expect(@tb.arregloColumnasEstaVacio()).to eq true
     end
-    it "no deberia estar vacia luego de dibujar" do
-      @tb.dibujarLinea()
-      expect(@tb.estaVacia()).to eq false
+    it "arreglo de filas no deberia estar vacio al insertar un elemento" do
+        @tb.insertarFila(0,0)
+        expect(@tb.arregloFilasEstaVacio()).to eq false
     end
+    it "arreglo de columnas no deberia estar vacio al insertar un elemento" do
+        @tb.insertarColumna(0,0)
+        expect(@tb.arregloColumnasEstaVacio()).to eq false
+    end
+
+    it "al insertar un elemento en la primera columna del tablero, se dibuja" do
+        @tb.insertarColumna(0,0)
+        expect(@tb.comprobarSiExisteColumnaDibujadaEnPos(0,0)).to eq true
+    end
+
+    it "llenar columnas y filas adyacentes para formar una 'caja' proporciona un punto" do
+        expect(@tb.insertarFila(1,2)).to eq 0
+        expect(@tb.insertarFila(1,3)).to eq 0
+        expect(@tb.insertarColumna(1,2)).to eq 0
+        expect(@tb.insertarColumna(2,2)).to eq 1
+    end
+
 end
