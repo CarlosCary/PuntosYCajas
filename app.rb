0b550634eb07b25b8297991e9f2badea7696318e
Harold <haroldcj95@gmail.com>
2018-11-24 19:00:54 -0400

    Fix feature/partida

--- app.rb
+++ app.rb
@@ -20,14 +20,13 @@ class App < Sinatra::Base
 
     get '/partidaJugando' do 
         @tam = 4
-        $tablero.convertirCoordenadasAFilasOColumnas(0, 0, 0, 1)
+        $tablero.insertarFilasOColumnas(0, 'arriba')
         @filas = $tablero.obtenerFilas
         @columnas = $tablero.obtenerColumnas
         erb :partida
     end
 
     post '/partidaJugando1' do
-        #$tablero.convertirCoordenadasAFilasOColumnas(params[:punto1PosX].to_i, params[:punto1PosY].to_i, params[:punto2PosX].to_i, params[:punto2PosY].to_i)
         $tablero.insertarFilasOColumnas(params[:numeroCaja].to_i, params[:direccionLinea].to_s)
         @filas = $tablero.obtenerFilas
         @columnas = $tablero.obtenerColumnas
