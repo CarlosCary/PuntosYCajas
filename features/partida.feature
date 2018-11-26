92bd5af0dc1b2b3ea4d1a2cdd455741d27d0565d
andres <abustamante@aitbol.com>
2018-11-26 02:49:53 -0400

    prueba para ganar puntos

--- features/partida.feature
+++ features/partida.feature
@@ -11,3 +11,8 @@ Feature: Tablero
         Given un jugador que esta en medio de una partida
         When ingresa el numero de caja "0" y la direccion de linea "arriba"
         Then se deberia dibujar una linea entre esos puntos
+
+    Scenario: jugador gana un punto
+        Given un juego con dos jugadores inicio
+        When ingresa la linea que completa cuadrado en la posicion "0" y la direccion de linea "arriba"
+        Then se deberia dibujar un "1" debajo de su nombre
--- features/step_definitions/partida_steps.rb
+++ features/step_definitions/partida_steps.rb
@@ -17,9 +17,35 @@ When("ingresa el numero de caja {string} y la direccion de linea {string}") do |
     click_on(id: 'realizarJugada')
 end
    
-   Then("se deberia dibujar una linea entre esos puntos") do
-     page.find('#tablero').first(".caja").has_css?('border-top-color:red')
-   end
-   
+Then("se deberia dibujar una linea entre esos puntos") do
+  page.find('#tablero').first(".caja").has_css?('border-top-color:red')
+end
+
+Given("un juego con dos jugadores inicio") do
+  page.driver.post('/iniciarPartida?tamTablero=4&numJugadores=2')
+end
+
+When("ingresa la linea que completa cuadrado en la posicion {string} y la direccion de linea {string}") do |caja, dirLinea|
+  page.driver.post('/partidaJugando1?numeroCaja=0&direccionLinea=abajo')
+  fill_in(id: 'numCaja', :with => 0)
+  page.select 'abajo', from: 'direccionLinea'
+  click_on(id: 'realizarJugada')
+  page.driver.post('/partidaJugando1?numeroCaja=0&direccionLinea=derecha')
+  fill_in(id: 'numCaja', :with => 0)
+  page.select 'derecha', from: 'direccionLinea'
+  click_on(id: 'realizarJugada')
+  page.driver.post('/partidaJugando1?numeroCaja=0&direccionLinea=izquierda')
+  fill_in(id: 'numCaja', :with => 0)
+  page.select 'izquierda', from: 'direccionLinea'
+  click_on(id: 'realizarJugada')
+  page.driver.post('/partidaJugando1?numeroCaja=caja&direccionLinea=dirLinea')
+  fill_in(id: 'numCaja', :with => caja)
+  page.select dirLinea, from: 'direccionLinea'
+  click_on(id: 'realizarJugada')
+end
+
+Then("se deberia dibujar un {string} debajo de su nombre") do |puntaje|
+  find_all('.puntaje').to_a.last.value() == puntaje
+end
 
 
